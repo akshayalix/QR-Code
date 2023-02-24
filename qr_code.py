2f93e959dd184e77a4478720066e9c0e4c699d90
akshayalix <akshayalix12@gmail.com>
2023-02-24 10:58:43 +0530

    reurn term

--- qr_code.py
+++ qr_code.py
@@ -21,6 +21,8 @@ def generateQR():
 
     base64_img = "data:image/png;base64," + b64encode(memory.getvalue()).decode('ascii')
 
+    return render_template('index.html', data=base64_img)
+
 
 if __name__ == '__main__':
     app.run(debug=True) 
