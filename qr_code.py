0fba64d5a46bf03e30078874af9e3ce823c1caa6
akshayalix <akshayalix12@gmail.com>
2023-02-24 10:55:36 +0530

    added method to save image in memory

--- qr_code.py
+++ qr_code.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import qrcode
+from io import BytesIO
+from base64 import b64encode
 
 app = Flask(__name__)
 
@@ -11,8 +13,14 @@ def home():
 @app.route('/', methods=['POST'])
 
 def generateQR():
+    memory = BytesIO()
     data = request.form.get('link')
     img = qrcode.make(data)
+    img.save(memory)
+    memory.seek(0)
+
+    base64_img = "data:image/png;base64," + b64encode(memory.getvalue()).decode('ascii')
+
 
 if __name__ == '__main__':
     app.run(debug=True) 
